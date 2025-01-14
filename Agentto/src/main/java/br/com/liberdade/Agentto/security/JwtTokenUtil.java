0be302442ebb795710a5e929872017b4bc45f559
renatocrachmad <162373494+renatocrachmad@users.noreply.github.com>
2025-01-14 16:59:55 -0300

    fix: atualiza o jwt

--- Agentto/src/main/java/br/com/liberdade/Agentto/security/JwtTokenUtil.java
+++ Agentto/src/main/java/br/com/liberdade/Agentto/security/JwtTokenUtil.java
@@ -10,21 +10,16 @@ import java.util.Date;
 @Component
 public class JwtTokenUtil {
 
-    private static final String SECRET_KEY = "suaChaveSecreta"; // Substitua por uma chave mais segura
+    private static final String SECRET_KEY = "suaChaveSecreta"; 
     private static final long EXPIRATION_TIME = 86400000; // 1 dia em milissegundos
 
-    /**
-     * Gera um token JWT para o usuário fornecido.
-     *
-     * @param usuario o objeto do usuário
-     * @return o token JWT gerado
-     */
+    
     public String generateToken(Usuario usuario) {
         return Jwts.builder()
-                .setSubject(usuario.getLogin()) // Identifica o usuário pelo login
+                .setSubject(usuario.getLogin()) 
                 .setIssuedAt(new Date()) // Data de emissão
                 .setExpiration(new Date(System.currentTimeMillis() + EXPIRATION_TIME)) // Data de expiração
-                .signWith(SignatureAlgorithm.HS512, SECRET_KEY) // Algoritmo e chave secreta
+                .signWith(SignatureAlgorithm.HS512, SECRET_KEY) 
                 .compact();
     }
 }
